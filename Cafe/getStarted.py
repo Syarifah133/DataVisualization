59b9038cbaa16282730995c6a8205b0e5af53e9d
Syarifah133 <141218418+Syarifah133@users.noreply.github.com>
2024-11-24 22:32:27 +0800

    Update getStarted.py

--- Cafe/getStarted.py
+++ Cafe/getStarted.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def display_get_started():
     st.title("☕DeeVee Hiho Cafe☕")
-    st.image("logo.png")
+    st.image("Cafe/logo.png")
     if st.button("Get Started"):
         st.session_state["page"] = "Sign In"
 
